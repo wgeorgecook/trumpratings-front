97313a39df1b83be2daac0516e3834afa701c6d0
William George Cook <wgeorgecook@gmail.com>
2018-11-28 14:19:13 -0800

    Add detail to approval/disapproval text

--- src/Page.js
+++ src/Page.js
@@ -22,13 +22,13 @@ class Page extends Component {
                 <h1> No tweet data available. </h1>
             </div> :
             <div id="tweetData">
-                <h1>Tweet data below: </h1>
+                <h1>Latest tweets:</h1>
                 <ul className="tweets">
                     { this.state.tweetInfo.map( (tweet, index) =>
-                        <li key={index}>
+                        <li key={index} className="tweet">
                             <div>
-                                {(tweet.approval) ? <span className='approval'>{ tweet.approval }</span> : <span className='approval'>No approval data</span>}
-                                {(tweet.disapproval) ? <span className='disapproval'>{ tweet.disapproval }</span> : <span className='disapproval'>No disapproval data</span>}
+                                {(tweet.approval) ? <span className='approval'>Approval Rating: { tweet.approval }%</span> : <span className='approval'>No approval data</span>}
+                                {(tweet.disapproval) ? <span className='disapproval'>Disapproval Rating: { tweet.disapproval }%</span> : <span className='disapproval'>No disapproval data</span>}
                                 <div className="tweetDate"> Posted on: { tweet.date_posted } </div>
                                 <div className="tweetText"> {tweet.tweet_text } </div>
                                 <div className='tweetUrl'>{ tweet.tweet_url }</div>
