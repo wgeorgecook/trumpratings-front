9069d0df012ff7bce84eef6c08febd3e21459527
William George Cook <wgeorgecook@gmail.com>
2018-12-14 20:49:36 -0800

    Fix api link in fetch

--- src/apiData.js
+++ src/apiData.js
@@ -1,11 +1,11 @@
 export const getAll = () =>
   // fetch("http://0.0.0.0:5000/api/v1/resources/all") // local
-  fetch("http://api.williamgeorgecook.com/api/v1/resources/all") // remote
+  fetch("http://superstubby.ddns.net/api/v1/resources/all") // remote
     .then(res => res.json())
     .then(data => data)
 
 export const getLatest = () =>
   // fetch("http://0.0.0.0:5000/api/v1/resources/latest") // local
-  fetch("http://api.williamgeorgecook.com/api/v1/resources/latest") // remote
+  fetch("http://superstubby.ddns.net/api/v1/resources/latest") // remote
     .then(res => res.json())
     .then(data => data)
